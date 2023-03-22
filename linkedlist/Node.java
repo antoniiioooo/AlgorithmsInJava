fb654bed66433987ffb3e70e31188a5e011c50f2
Antonio <103530496+antoniiioooo@users.noreply.github.com>
2023-03-21 18:57:59 -0700

    Update Node.java

--- linkedlist/Node.java
+++ linkedlist/Node.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Antonio O.
+ * Advanced Data Structures 282
+ * 
  */
 package linkedlist;
 
